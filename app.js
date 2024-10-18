bed706658fa35ab61e8583a2411d59ba3dfdd7f7
Anas Jawed <anasjawedbmj@gmail.com>
2024-10-18 20:07:31 +0300

    CalculatorIn JavascriptASsigneent

---
+++ app.js
@@ -0,0 +1,21 @@
+// function calculator(){
+//     let num = +prompt("Enter 1 Number");
+//     let num1 = +prompt("Enter 2 Number");
+//     let Operator = prompt("Enter Operator Number", "+,*,/,-");
+//     if(Operator === "+"){
+//         alert(num + num1);
+//     } else if(Operator === "-"){
+//         alert(num - num1);
+//     } else if(Operator === "*"){
+//         alert(num * num);
+//     } else if(Operator === "/"){
+//         if(Operator !== 0){
+//             alert("Divided by zero is not allowed");
+//         } else if(Operator === "/"){
+//                alert(num / num1);
+//         }
+//     } else{
+//         alert("Wrong Number");
+//     }
+
+// }
